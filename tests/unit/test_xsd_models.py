90770bf2a7e921f3572bc61d7d79a7e70b2ab84c
Lachlan Watson <lachlan.watson@gmail.com>
2024-08-29 14:15:28 +1000

    Beginning some basic validation, parameterisation works but most tests fail, I think I am using asserticle incorrectly

--- tests/unit/test_xsd_models.py
+++ tests/unit/test_xsd_models.py
@@ -1,8 +1,11 @@
 import pytest
 import inspect
-import os
 import importlib
 import pkgutil
+import pydantic_xml
+from itertools import product
+from assertical.fake.generator import generate_class_instance
+from lxml import etree
 
 
 def import_all_classes_from_module(package_name: str) -> dict:
@@ -31,14 +34,40 @@ def import_all_classes_from_module(package_name: str) -> dict:
     return classes_list
 
 
+def true_false_tuple(class_list: list) -> list[tuple[str, bool]]:
+    """Create list of tuples where optional_is_none provides either True or False values to the fixture"""
+    pairs = [[(item, True), (item, False)] for item in class_list]
+    return list(product(*pairs))
+
+
 @pytest.mark.parametrize("xml_class", import_all_classes_from_module("envoy_schema.server.schema"))
-def test_validate_xml_model(xml_class: type):
-    assert isinstance(xml_class, type)
+def test_validate_xml_model_csip_aus(xml_class: type, csip_aus_schema: etree.XMLSchema, use_assertical_extensions):
+    # Generate XML string
+    entity: xml_class = generate_class_instance(
+        xml_class,
+        optional_is_none=True,
+        type=None,
+    )
+
+    xml = entity.to_xml(skip_empty=True).decode()
+    xml_doc = etree.fromstring(xml)
+
+    is_valid = csip_aus_schema.validate(xml_doc)
+    errors = "\n".join((f"{e.line}: {e.message}" for e in csip_aus_schema.error_log))
+    assert is_valid, f"{xml}\nErrors:\n{errors}"
 
 
-# def run():
-#     class_list = [t for (name, t) in inspect.getmembers(xml_models, inspect.isclass)]
-#     print(len(class_list))
+# Screwing around:
+# class_list = import_all_classes_from_module("envoy_schema.server.schema")
+# print((class_list[0]))
 
+# print(class_list[1].__class__)
 
-# run()
+# for xml_class in class_list:
+#     # Filter for only xml models (not enum, flag etc)
+#     if issubclass(xml_class.__class__, pydantic_xml.model.XmlModelMeta):
+#         ns = xml_class.__xml_ns__
+#         if ns is not None:
+#             print(ns)
+#         else:
+#             print(xml_class.__xml_nsmap__)
