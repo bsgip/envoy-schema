03704712dbd04333d8a798cbfbf996eedef2e987
LachlanJW <watsonjlachlan@gmail.com>
2025-06-13 14:42:46 +1000

    Shortened the schema to make it more fit for purpose and simple

--- src/envoy_schema/admin/schema/site_reading.py
+++ src/envoy_schema/admin/schema/site_reading.py
@@ -1,53 +1,18 @@
 from datetime import datetime
-from typing import Optional
+from decimal import Decimal
 
 from pydantic import BaseModel
-
-from envoy_schema.server.schema.sep2.types import (
-    AccumulationBehaviourType,
-    DataQualifierType,
-    FlowDirectionType,
-    KindType,
-    PhaseCode,
-    QualityFlagsType,
-    RoleFlagsType,
-    UomType,
-)
 
 
 class AdminSiteReading(BaseModel):
     """
-    Admin representation of a site reading with embedded reading type metadata.
-
-    This class combines data from SiteReading and SiteReadingType models for convenience without requiring separate
-    queries or multiple objects.
+    Admin representation of a site reading.
+    For now this is kept simple as only active power watts.
     """
 
-    # Reading data
-    site_reading_id: int
-    site_id: int
-    time_period_start: datetime
-    time_period_seconds: int
-    value: int
-    local_id: Optional[int]
-    quality_flags: QualityFlagsType
-    reading_created_time: datetime
-    reading_changed_time: datetime
-
-    # Flattened reading type metadata
-    site_reading_type_id: int
-    aggregator_id: int
-    uom: UomType
-    data_qualifier: DataQualifierType
-    flow_direction: FlowDirectionType
-    accumulation_behaviour: AccumulationBehaviourType
-    kind: KindType
-    phase: PhaseCode
-    power_of_ten_multiplier: int
-    default_interval_seconds: int
-    role_flags: RoleFlagsType
-    reading_type_created_time: datetime
-    reading_type_changed_time: datetime
+    reading_start_time: datetime
+    duration_seconds: int
+    active_watts_sum: Decimal
 
 
 class AdminSiteReadingPageResponse(BaseModel):
