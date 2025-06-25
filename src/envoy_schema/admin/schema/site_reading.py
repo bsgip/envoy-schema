59b85713e69cb1de425ce893db82e8d3483f2105
LachlanJW <watsonjlachlan@gmail.com>
2025-06-25 15:16:16 +1000

    Changes to the Admin CSIP readings query

--- src/envoy_schema/admin/schema/site_reading.py
+++ src/envoy_schema/admin/schema/site_reading.py
@@ -5,13 +5,25 @@ from enum import IntEnum
 from pydantic import BaseModel
 
 
+class PhaseEnum(IntEnum):
+    """Condensed csip-aus compliant phase enum of the sep2 types."""
+
+    NA = 0
+    AN = 129
+    BN = 65
+    CN = 33
+
+
 class CSIPAusSiteReadingUnit(IntEnum):
     """
     This enum aims to capture allowed csip aus compliant readings for a site as a single value.
 
+    There is some flexibility built in for readings which aren't perfectly compliant (e.g. Kind or data qualifier of 0).
+    A future release might include a strict bool flag to enable the use of imperfect readings.
+
     It assumes the following:
-        - The Kind is 37 (POWER) or 0 (NA)
-        - The data qualifier is 2 (Average) or 0 (NA)
+        - The Kind is 37 (POWER, mandatory by csip) or 0 (NA)
+        - The data qualifier is 2 (Average, mandatory by csip) or 0 (NA)
         - Any RoleFlags are allowed, does not show whether sites are mirrors, DER, submeter, or aggregation points.
             If both site and device information are present (role flags 3 and 73, respectively, site data is returned)
         - Load convention applies: positive is import from the grid. If FlowDirectionType is stored it will be used,
@@ -22,11 +34,7 @@ class CSIPAusSiteReadingUnit(IntEnum):
     ACTIVEPOWER = 1  # W, UOM =  38
     REACTIVEPOWER = 2  # Var, UOM = 63
     FREQUENCY = 3  # Hz, UOM = 33
-
-    SINGLEPHASEVOLTAGE = 4  # V, UOM = 29, Phase code = 0 (NA)
-    VOLTAGEPHASEAN = 5  # V, UOM = 29, Phase code = 129 (AN)
-    VOLTAGEPHASEBN = 6  # V, UOM = 29, Phase code  = 65 (BN)
-    VOLTAGEPHASECN = 7  # V, UOM = 29, Phase code  = 33 (CN)
+    VOLTAGE = 4  # V, UOM = 29
 
 
 class CSIPAusSiteReading(BaseModel):
@@ -36,7 +44,9 @@ class CSIPAusSiteReading(BaseModel):
 
     reading_start_time: datetime
     duration_seconds: int
+    phase: PhaseEnum
     value: Decimal
+    csip_aus_unit: CSIPAusSiteReadingUnit  # # CSIP Aus unit enum used for the query
 
 
 class CSIPAusSiteReadingPageResponse(BaseModel):
@@ -46,7 +56,6 @@ class CSIPAusSiteReadingPageResponse(BaseModel):
     limit: int  # Maximum number of readings that could be returned
     start: int  # Number of readings skipped for pagination
     site_id: int  # Site ID filter used in the query
-    csip_aus_unit: CSIPAusSiteReadingUnit  # # CSIP Aus unit enum used for the query
     start_time: datetime  # Start time filter used in the query
     end_time: datetime  # End time filter used in the query
     readings: list[CSIPAusSiteReading]  # The readings in this page
